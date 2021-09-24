92f0399b11a1800a6f9143a5932a495476d51adc
Paolo <Paolo@DESKTOP-EO3FFOD>
2021-09-24 17:41:11 -0500

    Agregar archivos de proyecto.

---
+++ AppCollectionView/Models/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCollectionView.Models
+{
+    public class Students
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Telephone {get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ AppCollectionView/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AppCollectionView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppCollectionView.ViewModels
+{
+    public class StudentsViewModel
+    {
+        public ObservableCollection<Students> students { get; set; }
+
+        public StudentsViewModel()
+        {
+            students = new ObservableCollection<Students>
+            {
+                new Students{Name="Maria",LastName="Rojas", Telephone="(01) 999 999 998", Image="mujer.png"},
+                new Students{Name="Jose",LastName="Rojas", Telephone="(01) 999 999 999", Image="hombre.png"},
+                new Students{Name="Carlos",LastName="Rojas", Telephone="(01) 999 999 999",Image="hombre.png"},
+                new Students{Name="Mario",LastName="Rojas", Telephone="(01) 999 999 999",Image="hombre.png"},
+                new Students{Name="Roberto",LastName="Rojas", Telephone="(01) 999 999 999",Image="hombre.png"},
+                new Students{Name="Gloria",LastName="Rojas", Telephone="(01) 999 999 999",Image="mujer.png"},
+                new Students{Name="Rosario",LastName="Rojas", Telephone="(01) 999 999 999",Image="mujer.png"},
+                new Students{Name="Liliana",LastName="Rojas", Telephone="(01) 999 999 999",Image="mujer.png"},
+                new Students{Name="Guisella",LastName="Rojas", Telephone="(01) 999 999 999",Image="mujer.png"},
+                new Students{Name="Yessica",LastName="Rojas", Telephone="(01) 999 999 999",Image="mujer.png"},
+                new Students{Name="Luis",LastName="Rojas", Telephone="(01) 999 999 999",Image="hombre.png"},
+            };
+        }
+    }
+}
